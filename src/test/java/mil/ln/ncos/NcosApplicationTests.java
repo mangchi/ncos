aec98448cf7e4ca2afdf7336dcb9d3fed07223ce
강동호 <mangchi.kang@gmail.com>
2024-06-03 06:31:10 +0000

    Update src/test/java/mil/ln/ncos/NcosApplicationTests.java

--- src/test/java/mil/ln/ncos/NcosApplicationTests.java
+++ src/test/java/mil/ln/ncos/NcosApplicationTests.java
@@ -2,13 +2,10 @@ package mil.ln.ncos;
 
 import org.junit.jupiter.api.Test;
 //import org.springframework.boot.test.context.SpringBootTest;
-
 //@SpringBootTest
 class NcosApplicationTests {
-
 	@Test
 	void contextLoads() {
 		
 	}
-
 }

e9649d4bf7e4bf77ea047ae0728f280e544391e0
mangchi <mangchi.kang@gmail.com>
2024-09-26 06:58:12 +0900

    Update UserServiceImpl.java

--- src/main/java/mil/ln/ncos/user/service/UserServiceImpl.java
+++ src/main/java/mil/ln/ncos/user/service/UserServiceImpl.java
@@ -10,13 +10,62 @@ import mil.ln.ncos.cmmn.util.CryptoUtil;
 import mil.ln.ncos.dao.DAO;
 import mil.ln.ncos.user.vo.UserVo;
 
+import mil.ln.ncos.user.vo.CustomUserVo;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
 @RequiredArgsConstructor
 @Service
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService,UserDetailsService  {
 	
 	private final DAO dao;
 	//private final CacheService cacheService;
 	//private final AuthService authService;
+
+
+	/**
+	 * Spring Security 필수 메소드 구현
+     * @param username 
+     * @return UserDetails
+     * @throws UsernameNotFoundException 유저가 없을 때 예외 발생
+	 */
+	//@SuppressWarnings("unchecked")
+	@Override
+	public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException,AuthenticationServiceException{
+		CustomUserVo user = (UserVo)dao.selectOne("User.selectUser", userId);
+		if(user == null) {
+			throw new UsernameNotFoundException(messageSource.getMessage("error.userNotFound"));
+		}
+		
+		Map<String, PwFailCacheData> pwdFailCacheData = cacheService.getPwdFailCacheData(pwFailKey);
+		int failCnt = pwdFailCacheData.containsKey(user.getUserId()) ? pwdFailCacheData.get(user.getUserId()).getPwFailCnt() : 0;
+        if (failCnt >= pwFailCnt) {
+			if(!LocalDateTime.now().isAfter(pwdFailCacheData.get(user.getUserId()).getPwFailDateTime().plusMinutes(pwFaileLockMin))) {
+				throw new AuthenticationServiceException(messageSource.getMessage("error.userLockedByError",new String[] { String.valueOf(pwFaileLockMin) }));
+			}
+		}
+
+		user.setUserPw(user.getUserPw().replace("&#36;", "$"));
+		//List<String> roles = (List<String>)dao.selectList("User.selectUserRole", userId);
+		List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>(); 
+		authorities.add(new SimpleGrantedAuthority(user.getAuthority().name())); 
+		/*
+		for (String authority : roles) 
+		{ 
+			authorities.add(new SimpleGrantedAuthority(authority)); 
+			user.setAuthority(Authority.valueOf(authority));
+		} 
+		*/
+		user.setAuthorities(authorities);
+		return user;
+	}
 	
 	@SuppressWarnings("unchecked")
 	@Override
